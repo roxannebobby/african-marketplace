862cd2666f03a0012f633e75016b26764bec0840
Roxanne Weber <marie.sankofa@gmail.com>
2022-03-10 10:48:59 -0600

    loader and message added to product details page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 import Rating from '../components/Rating';
 import { listProductDetails } from '../actions/productActions';
 
@@ -20,96 +22,103 @@ const ProductScreen = ({ match }) => {
 			<Link className='btn btn-dark my-3' to='/shop'>
 				Go Back
 			</Link>
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant='danger'>{error}</Message>
+			) : (
+				<Row>
+					<Col md={3}>
+						<Image src={product.image} alt={product.name} fluid />
+						<Card>
+							<ListGroup variant='flush'>
+								<ListGroup.Item>
+									<Row>
+										<Col>Region:</Col>
+										<Col>
+											<strong>{product.region}</strong>
+										</Col>
+									</Row>
+								</ListGroup.Item>
 
-			<Row>
-				<Col md={3}>
-					<Image src={product.image} alt={product.name} fluid />
-					<Card>
+								<ListGroup.Item>
+									<Row>
+										<Col>Material(s):</Col>
+										<Col>
+											<strong>{product.materials}</strong>
+										</Col>
+									</Row>
+								</ListGroup.Item>
+
+								<ListGroup.Item>
+									<Row>
+										<Col>Size:</Col>
+										<Col>
+											<strong>{product.size}</strong>
+										</Col>
+									</Row>
+								</ListGroup.Item>
+							</ListGroup>
+						</Card>
+					</Col>
+
+					<Col md={4}>
 						<ListGroup variant='flush'>
 							<ListGroup.Item>
-								<Row>
-									<Col>Region:</Col>
-									<Col>
-										<strong>{product.region}</strong>
-									</Col>
-								</Row>
+								<h2>{product.name}</h2>
 							</ListGroup.Item>
 
 							<ListGroup.Item>
-								<Row>
-									<Col>Material(s):</Col>
-									<Col>
-										<strong>{product.materials}</strong>
-									</Col>
-								</Row>
+								<Rating
+									value={product.rating}
+									text={`${product.numReviews} reviews`}
+								/>
 							</ListGroup.Item>
 
+							<ListGroup.Item>Price: $ {product.price}</ListGroup.Item>
+
 							<ListGroup.Item>
-								<Row>
-									<Col>Size:</Col>
-									<Col>
-										<strong>{product.size}</strong>
-									</Col>
-								</Row>
+								Description: {product.description}
 							</ListGroup.Item>
 						</ListGroup>
-					</Card>
-				</Col>
+					</Col>
+					<Col md={4}>
+						<Card>
+							<ListGroup variant='flush'>
+								<ListGroup.Item>
+									<Row>
+										<Col>Price:</Col>
+										<Col>
+											<strong>$ {product.price}</strong>
+										</Col>
+									</Row>
+								</ListGroup.Item>
 
-				<Col md={4}>
-					<ListGroup variant='flush'>
-						<ListGroup.Item>
-							<h2>{product.name}</h2>
-						</ListGroup.Item>
+								<ListGroup.Item>
+									<Row>
+										<Col>Status:</Col>
+										<Col>
+											<strong>
+												{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+											</strong>
+										</Col>
+									</Row>
 
-						<ListGroup.Item>
-							<Rating
-								value={product.rating}
-								text={`${product.numReviews} reviews`}
-							/>
-						</ListGroup.Item>
-
-						<ListGroup.Item>Price: $ {product.price}</ListGroup.Item>
-
-						<ListGroup.Item>Description: {product.description}</ListGroup.Item>
-					</ListGroup>
-				</Col>
-				<Col md={4}>
-					<Card>
-						<ListGroup variant='flush'>
-							<ListGroup.Item>
-								<Row>
-									<Col>Price:</Col>
-									<Col>
-										<strong>$ {product.price}</strong>
-									</Col>
-								</Row>
-							</ListGroup.Item>
-
-							<ListGroup.Item>
-								<Row>
-									<Col>Status:</Col>
-									<Col>
-										<strong>
-											{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-										</strong>
-									</Col>
-								</Row>
-
-								<Row>
-									<Button
-										className='btn btn-primary btn-lg btn-block'
-										disabled={product.countInStock === 0}
-										type='button'
-									>
-										Add to Cart
-									</Button>
-								</Row>
-							</ListGroup.Item>
-						</ListGroup>
-					</Card>
-				</Col>
-			</Row>
+									<Row>
+										<Button
+											className='btn btn-primary btn-lg btn-block'
+											disabled={product.countInStock === 0}
+											type='button'
+										>
+											Add to Cart
+										</Button>
+									</Row>
+								</ListGroup.Item>
+							</ListGroup>
+						</Card>
+					</Col>
+				</Row>
+			)}
 		</>
 	);
 };
