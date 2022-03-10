0e01226ef8234eb440a836d2cde5b6f2766c9bc4
Roxanne Weber <marie.sankofa@gmail.com>
2022-03-10 12:22:25 -0600

    cart reducerJS added

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,15 +1,6 @@
-import {
-	CART_ADD_ITEM,
-	CART_REMOVE_ITEM,
-	CART_SAVE_SHIPPING_ADDRESS,
-	CART_SAVE_PAYMENT_METHOD,
-	CART_CLEAR_ITEMS,
-} from '../constants/cartConstants';
+import { CART_ADD_ITEM } from '../constants/cartConstants';
 
-export const cartReducer = (
-	state = { cartItems: [], shippingAddress: {} },
-	action
-) => {
+export const cartReducer = (state = { cartItems: [] }, action) => {
 	switch (action.type) {
 		case CART_ADD_ITEM:
 			const item = action.payload;
@@ -29,11 +20,6 @@ export const cartReducer = (
 					cartItems: [...state.cartItems, item],
 				};
 			}
-		case CART_REMOVE_ITEM:
-			return {
-				...state,
-				cartItems: state.cartItems.filter((x) => x.product !== action.payload),
-			};
 
 		default:
 			return state;
