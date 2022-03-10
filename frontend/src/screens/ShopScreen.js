e0935e66f4f6a12df0289e4488d64ba9797a9aea
Roxanne Weber <marie.sankofa@gmail.com>
2022-03-10 10:24:55 -0600

    message and loader components installed on shopscreen

--- frontend/src/screens/ShopScreen.js
+++ frontend/src/screens/ShopScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 import { listProducts } from '../actions/productActions';
 
 const ShopScreen = () => {
@@ -18,9 +20,9 @@ const ShopScreen = () => {
 		<>
 			<h3>Latest Products</h3>
 			{loading ? (
-				<h2>Loading...</h2>
+				<Loader />
 			) : error ? (
-				<h3>{error}</h3>
+				<Message varient='red'>{error}</Message>
 			) : (
 				<Row>
 					{products.map((product) => (
