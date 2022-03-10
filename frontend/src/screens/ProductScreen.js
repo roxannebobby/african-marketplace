6edbcb6a6576af6ae2bbd21873ed51e7aee9ff43
Roxanne Weber <marie.sankofa@gmail.com>
2022-03-10 11:05:50 -0600

    quantity form item added to productScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,13 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import {
+	Row,
+	Col,
+	Image,
+	ListGroup,
+	Card,
+	Button,
+	Form,
+} from 'react-bootstrap';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import Rating from '../components/Rating';
 import { listProductDetails } from '../actions/productActions';
 
 const ProductScreen = ({ match }) => {
+	const [qty, setQty] = useState(0);
+
 	const dispatch = useDispatch();
 
 	const productDetails = useSelector((state) => state.productDetails);
@@ -102,6 +112,30 @@ const ProductScreen = ({ match }) => {
 												{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
 											</strong>
 										</Col>
+									</Row>
+									<Row>
+										{product.countInStock > 0 && (
+											<ListGroup.Item>
+												<Row>
+													<Col>Qty</Col>
+													<Col>
+														<Form.Control
+															as='select'
+															value={qty}
+															onChange={(e) => setQty(e.target.value)}
+														>
+															{[...Array(product.countInStock).keys()].map(
+																(x) => (
+																	<option key={x + 1} value={x + 1}>
+																		{x + 1}
+																	</option>
+																)
+															)}
+														</Form.Control>
+													</Col>
+												</Row>
+											</ListGroup.Item>
+										)}
 									</Row>
 
 									<Row>
